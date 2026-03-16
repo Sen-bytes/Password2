8aa70a7c0797564b093010f61e08f09ded717a2d
sen-bytes <1549455@senati.pe>
2026-03-15 23:32:25 -0500

    Tarea1

--- app/src/main/java/com/example/password/Pass.kt
+++ app/src/main/java/com/example/password/Pass.kt
@@ -1,12 +1,11 @@
 package com.example.password
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doOnTextChanged // Importante agregar esta importación
+import androidx.core.widget.doOnTextChanged
 
 class Pass : AppCompatActivity() {
     private val MiViewModel: PassViewModel by viewModels()
@@ -22,9 +21,7 @@ class Pass : AppCompatActivity() {
             tv.text = Seguridad
         }
 
-        // NUEVO CÓDIGO: Escuchamos cada vez que el texto cambia en tiempo real
         et.doOnTextChanged { text, start, before, count ->
-            // Se ejecuta cada vez que el usuario teclea o borra un carácter
             MiViewModel.setPass(text.toString())
         }
 
